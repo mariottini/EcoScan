45f6b18256b49d726aa44135581828b20cda01f6
mariottini <matteomariottini9@gmail.com>
2025-03-24 17:20:48 +0100

    Upload new DB

---
+++ sketches/calendar_query.sql
@@ -0,0 +1,324 @@
+INSERT INTO calendar (day, id_category, id_week, id_city) VALUES
+-- GENNAIO 2025
+('venerdì', 3, 1, 2),    -- 3/1 SECCO (settimana 1: 30/12-5/1)
+('sabato', 2, 1, 2),     -- 4/1 UMIDO
+('lunedì', 1, 2, 2),     -- 6/1 CARTA (settimana 2: 6/1-12/1)
+('martedì', 2, 2, 2),    -- 7/1 UMIDO
+('mercoledì', 4, 2, 2),  -- 8/1 PLASTICA/METALLI
+('venerdì', 3, 2, 2),    -- 10/1 SECCO
+('sabato', 2, 2, 2),     -- 11/1 UMIDO
+('lunedì', 1, 3, 2),     -- 13/1 CARTA (settimana 3: 13/1-19/1)
+('martedì', 2, 3, 2),    -- 14/1 UMIDO
+('mercoledì', 4, 3, 2),  -- 15/1 PLASTICA/METALLI
+('venerdì', 3, 3, 2),    -- 17/1 SECCO
+('sabato', 2, 3, 2),     -- 18/1 UMIDO
+('lunedì', 1, 4, 2),     -- 20/1 CARTA (settimana 4: 20/1-26/1)
+('martedì', 2, 4, 2),    -- 21/1 UMIDO
+('mercoledì', 4, 4, 2),  -- 22/1 PLASTICA/METALLI
+('venerdì', 3, 4, 2),    -- 24/1 SECCO
+('sabato', 2, 4, 2),     -- 25/1 UMIDO
+('lunedì', 1, 5, 2),     -- 27/1 CARTA (settimana 5: 27/1-2/2)
+('martedì', 2, 5, 2),    -- 28/1 UMIDO
+('mercoledì', 4, 5, 2),  -- 29/1 PLASTICA/METALLI
+('venerdì', 3, 5, 2),    -- 31/1 SECCO
+
+-- FEBBRAIO 2025
+('sabato', 2, 5, 2),     -- 1/2 UMIDO
+('lunedì', 1, 6, 2),     -- 3/2 CARTA (settimana 6: 3/2-9/2)
+('martedì', 2, 6, 2),    -- 4/2 UMIDO
+('mercoledì', 4, 6, 2),  -- 5/2 PLASTICA/METALLI
+('venerdì', 3, 6, 2),    -- 7/2 SECCO
+('sabato', 2, 6, 2),     -- 8/2 UMIDO
+('lunedì', 1, 7, 2),     -- 10/2 CARTA (settimana 7: 10/2-16/2)
+('martedì', 2, 7, 2),    -- 11/2 UMIDO
+('mercoledì', 4, 7, 2),  -- 12/2 PLASTICA/METALLI
+('venerdì', 3, 7, 2),    -- 14/2 SECCO
+('sabato', 2, 7, 2),     -- 15/2 UMIDO
+('lunedì', 1, 8, 2),     -- 17/2 CARTA (settimana 8: 17/2-23/2)
+('martedì', 2, 8, 2),    -- 18/2 UMIDO
+('mercoledì', 4, 8, 2),  -- 19/2 PLASTICA/METALLI
+('venerdì', 3, 8, 2),    -- 21/2 SECCO
+('sabato', 2, 8, 2),     -- 22/2 UMIDO
+('lunedì', 1, 9, 2),     -- 24/2 CARTA (settimana 9: 24/2-2/3)
+('martedì', 2, 9, 2),    -- 25/2 UMIDO
+('mercoledì', 4, 9, 2),  -- 26/2 PLASTICA/METALLI
+('venerdì', 3, 9, 2),    -- 28/2 SECCO
+
+-- MARZO 2025
+('sabato', 2, 9, 2),     -- 1/3 UMIDO
+('lunedì', 1, 10, 2),    -- 3/3 CARTA (settimana 10: 3/3-9/3)
+('martedì', 2, 10, 2),   -- 4/3 UMIDO
+('mercoledì', 4, 10, 2), -- 5/3 PLASTICA/METALLI
+('venerdì', 3, 10, 2),   -- 7/3 SECCO
+('sabato', 2, 10, 2),    -- 8/3 UMIDO
+('lunedì', 1, 11, 2),    -- 10/3 CARTA (settimana 11: 10/3-16/3)
+('martedì', 2, 11, 2),   -- 11/3 UMIDO
+('mercoledì', 4, 11, 2), -- 12/3 PLASTICA/METALLI
+('venerdì', 3, 11, 2),   -- 14/3 SECCO
+('sabato', 2, 11, 2),    -- 15/3 UMIDO
+('lunedì', 1, 12, 2),    -- 17/3 CARTA (settimana 12: 17/3-23/3)
+('martedì', 2, 12, 2),   -- 18/3 UMIDO
+('mercoledì', 4, 12, 2), -- 19/3 PLASTICA/METALLI
+('venerdì', 3, 12, 2),   -- 21/3 SECCO
+('sabato', 2, 12, 2),    -- 22/3 UMIDO
+('lunedì', 1, 13, 2),    -- 24/3 CARTA (settimana 13: 24/3-30/3)
+('martedì', 2, 13, 2),   -- 25/3 UMIDO
+('mercoledì', 4, 13, 2), -- 26/3 PLASTICA/METALLI
+('venerdì', 3, 13, 2),   -- 28/3 SECCO
+('sabato', 2, 13, 2),    -- 29/3 UMIDO
+('lunedì', 1, 14, 2),    -- 31/3 CARTA (settimana 14: 31/3-6/4)
+
+-- APRILE 2025
+('martedì', 2, 14, 2),   -- 1/4 UMIDO
+('mercoledì', 4, 14, 2), -- 2/4 PLASTICA/METALLI
+('venerdì', 3, 14, 2),   -- 4/4 SECCO
+('sabato', 2, 14, 2),    -- 5/4 UMIDO
+('lunedì', 1, 15, 2),    -- 7/4 CARTA (settimana 15: 7/4-13/4)
+('martedì', 2, 15, 2),   -- 8/4 UMIDO
+('mercoledì', 4, 15, 2), -- 9/4 PLASTICA/METALLI
+('venerdì', 3, 15, 2),   -- 11/4 SECCO
+('sabato', 2, 15, 2),    -- 12/4 UMIDO
+('lunedì', 1, 16, 2),    -- 14/4 CARTA (settimana 16: 14/4-20/4)
+('martedì', 2, 16, 2),   -- 15/4 UMIDO
+('mercoledì', 4, 16, 2), -- 16/4 PLASTICA/METALLI
+('venerdì', 3, 16, 2),   -- 18/4 SECCO
+('sabato', 2, 16, 2),    -- 19/4 UMIDO
+('lunedì', 1, 17, 2),    -- 21/4 CARTA (settimana 17: 21/4-27/4)
+('martedì', 2, 17, 2),   -- 22/4 UMIDO
+('mercoledì', 4, 17, 2), -- 23/4 PLASTICA/METALLI
+('venerdì', 3, 17, 2),   -- 25/4 SECCO
+('sabato', 2, 17, 2),    -- 26/4 UMIDO
+('lunedì', 1, 18, 2),    -- 28/4 CARTA (settimana 18: 28/4-4/5)
+('martedì', 2, 18, 2),   -- 29/4 UMIDO
+('mercoledì', 4, 18, 2), -- 30/4 PLASTICA/METALLI
+
+-- MAGGIO 2025
+('venerdì', 3, 18, 2),   -- 2/5 SECCO
+('sabato', 2, 18, 2),    -- 3/5 UMIDO
+('lunedì', 1, 19, 2),    -- 5/5 CARTA (settimana 19: 5/5-11/5)
+('martedì', 2, 19, 2),   -- 6/5 UMIDO
+('mercoledì', 4, 19, 2), -- 7/5 PLASTICA/METALLI
+('venerdì', 3, 19, 2),   -- 9/5 SECCO
+('sabato', 2, 19, 2),    -- 10/5 UMIDO
+('lunedì', 1, 20, 2),    -- 12/5 CARTA (settimana 20: 12/5-18/5)
+('martedì', 2, 20, 2),   -- 13/5 UMIDO
+('mercoledì', 4, 20, 2), -- 14/5 PLASTICA/METALLI
+('venerdì', 3, 20, 2),   -- 16/5 SECCO
+('sabato', 2, 20, 2),    -- 17/5 UMIDO
+('lunedì', 1, 21, 2),    -- 19/5 CARTA (settimana 21: 19/5-25/5)
+('martedì', 2, 21, 2),   -- 20/5 UMIDO
+('mercoledì', 4, 21, 2), -- 21/5 PLASTICA/METALLI
+('venerdì', 3, 21, 2),   -- 23/5 SECCO
+('sabato', 2, 21, 2),    -- 24/5 UMIDO
+('lunedì', 1, 22, 2),    -- 26/5 CARTA (settimana 22: 26/5-1/6)
+('martedì', 2, 22, 2),   -- 27/5 UMIDO
+('mercoledì', 4, 22, 2), -- 28/5 PLASTICA/METALLI
+('venerdì', 3, 22, 2),   -- 30/5 SECCO
+('sabato', 2, 22, 2),    -- 31/5 UMIDO
+
+-- GIUGNO 2025
+('lunedì', 1, 23, 2),    -- 2/6 CARTA (settimana 23: 2/6-8/6)
+('martedì', 2, 23, 2),   -- 3/6 UMIDO
+('mercoledì', 4, 23, 2), -- 4/6 PLASTICA/METALLI
+('venerdì', 3, 23, 2),   -- 6/6 SECCO
+('sabato', 2, 23, 2),    -- 7/6 UMIDO
+('lunedì', 1, 24, 2),    -- 9/6 CARTA (settimana 24: 9/6-15/6)
+('martedì', 2, 24, 2),   -- 10/6 UMIDO
+('mercoledì', 4, 24, 2), -- 11/6 PLASTICA/METALLI
+('giovedì', 2, 24, 2),   -- 12/6 UMIDO (eccezione estiva)
+('venerdì', 3, 24, 2),   -- 13/6 SECCO
+('sabato', 2, 24, 2),    -- 14/6 UMIDO
+('lunedì', 1, 25, 2),    -- 16/6 CARTA (settimana 25: 16/6-22/6)
+('martedì', 2, 25, 2),   -- 17/6 UMIDO
+('mercoledì', 4, 25, 2), -- 18/6 PLASTICA/METALLI
+('giovedì', 2, 25, 2),   -- 19/6 UMIDO
+('venerdì', 3, 25, 2),   -- 20/6 SECCO
+('sabato', 2, 25, 2),    -- 21/6 UMIDO
+('lunedì', 1, 26, 2),    -- 23/6 CARTA (settimana 26: 23/6-29/6)
+('martedì', 2, 26, 2),   -- 24/6 UMIDO
+('mercoledì', 4, 26, 2), -- 25/6 PLASTICA/METALLI
+('giovedì', 2, 26, 2),   -- 26/6 UMIDO
+('venerdì', 3, 26, 2),   -- 27/6 SECCO
+('sabato', 2, 26, 2),    -- 28/6 UMIDO
+('lunedì', 1, 27, 2),    -- 30/6 CARTA (settimana 27: 30/6-6/7)
+
+-- LUGLIO 2025
+('martedì', 2, 27, 2),   -- 1/7 UMIDO
+('mercoledì', 4, 27, 2), -- 2/7 PLASTICA/METALLI
+('giovedì', 2, 27, 2),   -- 3/7 UMIDO
+('venerdì', 3, 27, 2),   -- 4/7 SECCO
+('sabato', 2, 27, 2),    -- 5/7 UMIDO
+('lunedì', 1, 28, 2),    -- 7/7 CARTA (settimana 28: 7/7-13/7)
+('martedì', 2, 28, 2),   -- 8/7 UMIDO
+('mercoledì', 4, 28, 2), -- 9/7 PLASTICA/METALLI
+('giovedì', 2, 28, 2),   -- 10/7 UMIDO
+('venerdì', 3, 28, 2),   -- 11/7 SECCO
+('sabato', 2, 28, 2),    -- 12/7 UMIDO
+('lunedì', 1, 29, 2),    -- 14/7 CARTA (settimana 29: 14/7-20/7)
+('martedì', 2, 29, 2),   -- 15/7 UMIDO
+('mercoledì', 4, 29, 2), -- 16/7 PLASTICA/METALLI
+('giovedì', 2, 29, 2),   -- 17/7 UMIDO
+('venerdì', 3, 29, 2),   -- 18/7 SECCO
+('sabato', 2, 29, 2),    -- 19/7 UMIDO
+('lunedì', 1, 30, 2),    -- 21/7 CARTA (settimana 30: 21/7-27/7)
+('martedì', 2, 30, 2),   -- 22/7 UMIDO
+('mercoledì', 4, 30, 2), -- 23/7 PLASTICA/METALLI
+('giovedì', 2, 30, 2),   -- 24/7 UMIDO
+('venerdì', 3, 30, 2),   -- 25/7 SECCO
+('sabato', 2, 30, 2),    -- 26/7 UMIDO
+('lunedì', 1, 31, 2),    -- 28/7 CARTA (settimana 31: 28/7-3/8)
+('martedì', 2, 31, 2),   -- 29/7 UMIDO
+('mercoledì', 4, 31, 2), -- 30/7 PLASTICA/METALLI
+('giovedì', 2, 31, 2),   -- 31/7 UMIDO
+
+-- AGOSTO 2025
+('venerdì', 3, 31, 2),    -- 1/8 SECCO (settimana 31: 28/7-3/8)
+('sabato', 2, 31, 2),     -- 2/8 UMIDO
+('lunedì', 1, 32, 2),     -- 4/8 CARTA (settimana 32: 4/8-10/8)
+('martedì', 2, 32, 2),    -- 5/8 UMIDO
+('mercoledì', 4, 32, 2),  -- 6/8 PLASTICA/METALLI
+('giovedì', 2, 32, 2),    -- 7/8 UMIDO
+('venerdì', 3, 32, 2),    -- 8/8 SECCO
+('sabato', 2, 32, 2),     -- 9/8 UMIDO
+('lunedì', 1, 33, 2),     -- 11/8 CARTA (settimana 33: 11/8-17/8)
+('martedì', 2, 33, 2),    -- 12/8 UMIDO
+('mercoledì', 4, 33, 2),  -- 13/8 PLASTICA/METALLI
+('giovedì', 2, 33, 2),    -- 14/8 UMIDO
+('venerdì', 3, 33, 2),    -- 15/8 SECCO
+('sabato', 2, 33, 2),     -- 16/8 UMIDO
+('lunedì', 1, 34, 2),     -- 18/8 CARTA (settimana 34: 18/8-24/8)
+('martedì', 2, 34, 2),    -- 19/8 UMIDO
+('mercoledì', 4, 34, 2),  -- 20/8 PLASTICA/METALLI
+('giovedì', 2, 34, 2),    -- 21/8 UMIDO
+('venerdì', 3, 34, 2),    -- 22/8 SECCO
+('sabato', 2, 34, 2),     -- 23/8 UMIDO
+('lunedì', 1, 35, 2),     -- 25/8 CARTA (settimana 35: 25/8-31/8)
+('martedì', 2, 35, 2),    -- 26/8 UMIDO
+('mercoledì', 4, 35, 2),  -- 27/8 PLASTICA/METALLI
+('giovedì', 2, 35, 2),    -- 28/8 UMIDO
+('venerdì', 3, 35, 2),    -- 29/8 SECCO
+('sabato', 2, 35, 2),     -- 30/8 UMIDO
+
+-- SETTEMBRE 2025
+('lunedì', 1, 36, 2),     -- 1/9 CARTA (settimana 36: 1/9-7/9)
+('martedì', 2, 36, 2),    -- 2/9 UMIDO
+('mercoledì', 4, 36, 2),  -- 3/9 PLASTICA/METALLI
+('giovedì', 2, 36, 2),    -- 4/9 UMIDO
+('venerdì', 3, 36, 2),    -- 5/9 SECCO
+('sabato', 2, 36, 2),     -- 6/9 UMIDO
+('lunedì', 1, 37, 2),     -- 8/9 CARTA (settimana 37: 8/9-14/9)
+('martedì', 2, 37, 2),    -- 9/9 UMIDO
+('mercoledì', 4, 37, 2),  -- 10/9 PLASTICA/METALLI
+('giovedì', 2, 37, 2),    -- 11/9 UMIDO
+('venerdì', 3, 37, 2),    -- 12/9 SECCO
+('sabato', 2, 37, 2),     -- 13/9 UMIDO
+('lunedì', 1, 38, 2),     -- 15/9 CARTA (settimana 38: 15/9-21/9)
+('martedì', 2, 38, 2),    -- 16/9 UMIDO
+('mercoledì', 4, 38, 2),  -- 17/9 PLASTICA/METALLI
+('venerdì', 3, 38, 2),    -- 19/9 SECCO
+('sabato', 2, 38, 2),     -- 20/9 UMIDO
+('lunedì', 1, 39, 2),     -- 22/9 CARTA (settimana 39: 22/9-28/9)
+('martedì', 2, 39, 2),    -- 23/9 UMIDO
+('mercoledì', 4, 39, 2),  -- 24/9 PLASTICA/METALLI
+('venerdì', 3, 39, 2),    -- 26/9 SECCO
+('sabato', 2, 39, 2),     -- 27/9 UMIDO
+('lunedì', 1, 40, 2),     -- 29/9 CARTA (settimana 40: 29/9-5/10)
+('martedì', 2, 40, 2),    -- 30/9 UMIDO
+
+-- OTTOBRE 2025
+('mercoledì', 4, 40, 2),  -- 1/10 PLASTICA/METALLI
+('venerdì', 3, 40, 2),    -- 3/10 SECCO
+('sabato', 2, 40, 2),     -- 4/10 UMIDO
+('lunedì', 1, 41, 2),     -- 6/10 CARTA (settimana 41: 6/10-12/10)
+('martedì', 2, 41, 2),    -- 7/10 UMIDO
+('mercoledì', 4, 41, 2),  -- 8/10 PLASTICA/METALLI
+('venerdì', 3, 41, 2),    -- 10/10 SECCO
+('sabato', 2, 41, 2),     -- 11/10 UMIDO
+('lunedì', 1, 42, 2),     -- 13/10 CARTA (settimana 42: 13/10-19/10)
+('martedì', 2, 42, 2),    -- 14/10 UMIDO
+('mercoledì', 4, 42, 2),  -- 15/10 PLASTICA/METALLI
+('venerdì', 3, 42, 2),    -- 17/10 SECCO
+('sabato', 2, 42, 2),     -- 18/10 UMIDO
+('lunedì', 1, 43, 2),     -- 20/10 CARTA (settimana 43: 20/10-26/10)
+('martedì', 2, 43, 2),    -- 21/10 UMIDO
+('mercoledì', 4, 43, 2),  -- 22/10 PLASTICA/METALLI
+('venerdì', 3, 43, 2),    -- 24/10 SECCO
+('sabato', 2, 43, 2),     -- 25/10 UMIDO
+('lunedì', 1, 44, 2),     -- 27/10 CARTA (settimana 44: 27/10-2/11)
+('martedì', 2, 44, 2),    -- 28/10 UMIDO
+('mercoledì', 4, 44, 2),  -- 29/10 PLASTICA/METALLI
+('venerdì', 3, 44, 2),    -- 31/10 SECCO
+
+-- NOVEMBRE 2025
+('sabato', 2, 44, 2),     -- 1/11 UMIDO
+('lunedì', 1, 45, 2),     -- 3/11 CARTA (settimana 45: 3/11-9/11)
+('martedì', 2, 45, 2),    -- 4/11 UMIDO
+('mercoledì', 4, 45, 2),  -- 5/11 PLASTICA/METALLI
+('venerdì', 3, 45, 2),    -- 7/11 SECCO
+('sabato', 2, 45, 2),     -- 8/11 UMIDO
+('lunedì', 1, 46, 2),     -- 10/11 CARTA (settimana 46: 10/11-16/11)
+('martedì', 2, 46, 2),    -- 11/11 UMIDO
+('mercoledì', 4, 46, 2),  -- 12/11 PLASTICA/METALLI
+('venerdì', 3, 46, 2),    -- 14/11 SECCO
+('sabato', 2, 46, 2),     -- 15/11 UMIDO
+('lunedì', 1, 47, 2),     -- 17/11 CARTA (settimana 47: 17/11-23/11)
+('martedì', 2, 47, 2),    -- 18/11 UMIDO
+('mercoledì', 4, 47, 2),  -- 19/11 PLASTICA/METALLI
+('venerdì', 3, 47, 2),    -- 21/11 SECCO
+('sabato', 2, 47, 2),     -- 22/11 UMIDO
+('lunedì', 1, 48, 2),     -- 24/11 CARTA (settimana 48: 24/11-30/11)
+('martedì', 2, 48, 2),    -- 25/11 UMIDO
+('mercoledì', 4, 48, 2),  -- 26/11 PLASTICA/METALLI
+('venerdì', 3, 48, 2),    -- 28/11 SECCO
+('sabato', 2, 48, 2),     -- 29/11 UMIDO
+('lunedì', 1, 48, 2),     -- 29/11 CARTA (duplicato, data errata)
+('martedì', 2, 49, 2),    -- 30/11 UMIDO (settimana 49: 1/12-7/12)
+
+-- DICEMBRE 2025
+('lunedì', 1, 49, 2),     -- 1/12 CARTA
+('martedì', 2, 49, 2),    -- 2/12 UMIDO
+('mercoledì', 4, 49, 2),  -- 3/12 PLASTICA/METALLI
+('venerdì', 3, 49, 2),    -- 5/12 SECCO
+('sabato', 2, 49, 2),     -- 6/12 UMIDO
+('lunedì', 1, 50, 2),     -- 8/12 CARTA (settimana 50: 8/12-14/12)
+('martedì', 2, 50, 2),    -- 9/12 UMIDO
+('mercoledì', 4, 50, 2),  -- 10/12 PLASTICA/METALLI
+('venerdì', 3, 50, 2),    -- 12/12 SECCO
+('sabato', 2, 50, 2),     -- 13/12 UMIDO
+('lunedì', 1, 51, 2),     -- 15/12 CARTA (settimana 51: 15/12-21/12)
+('martedì', 2, 51, 2),    -- 16/12 UMIDO
+('mercoledì', 4, 51, 2),  -- 17/12 PLASTICA/METALLI
+('venerdì', 3, 51, 2),    -- 19/12 SECCO
+('sabato', 2, 51, 2),     -- 20/12 UMIDO
+('lunedì', 1, 52, 2),     -- 22/12 CARTA (settimana 52: 22/12-28/12)
+('martedì', 2, 52, 2),    -- 23/12 UMIDO
+('mercoledì', 4, 52, 2),  -- 24/12 PLASTICA/METALLI
+('venerdì', 3, 52, 2),    -- 26/12 SECCO
+('sabato', 2, 52, 2),     -- 27/12 UMIDO
+('lunedì', 1, 53, 2),      -- 29/12 CARTA (settimana 1: 29/12-4/1)
+('martedì', 2, 53, 2),     -- 30/12 UMIDO
+('mercoledì', 4, 53, 2),   -- 31/12 PLASTICA/METALLI
+
+-- GENNAIO 2026
+('venerdì', 3, 53, 2),     -- 2/1 SECCO
+('sabato', 2, 53, 2),      -- 3/1 UMIDO
+('lunedì', 1, 54, 2),      -- 5/1 CARTA (settimana 2: 5/1-11/1)
+('martedì', 2, 54, 2),     -- 6/1 UMIDO
+('mercoledì', 4, 54, 2),   -- 7/1 PLASTICA/METALLI
+('venerdì', 3, 54, 2),     -- 9/1 SECCO
+('sabato', 2, 54, 2),      -- 10/1 UMIDO
+('lunedì', 1, 55, 2),      -- 12/1 CARTA (settimana 3: 12/1-18/1)
+('martedì', 2, 55, 2),     -- 13/1 UMIDO
+('mercoledì', 4, 55, 2),   -- 14/1 PLASTICA/METALLI
+('venerdì', 3, 55, 2),     -- 16/1 SECCO
+('sabato', 2, 55, 2),      -- 17/1 UMIDO
+('lunedì', 1, 56, 2),      -- 19/1 CARTA (settimana 4: 19/1-25/1)
+('martedì', 2, 56, 2),     -- 20/1 UMIDO
+('mercoledì', 4, 56, 2),   -- 21/1 PLASTICA/METALLI
+('venerdì', 3, 56, 2),     -- 23/1 SECCO
+('sabato', 2, 56, 2),      -- 24/1 UMIDO
+('lunedì', 1, 57, 2),      -- 26/1 CARTA (settimana 5: 26/1-1/2)
+('martedì', 2, 57, 2),     -- 27/1 UMIDO
+('mercoledì', 4, 57, 2),   -- 28/1 PLASTICA/METALLI
+('venerdì', 3, 57, 2),     -- 30/1 SECCO
+('sabato', 2, 57, 2);      -- 31/1 UMIDO
