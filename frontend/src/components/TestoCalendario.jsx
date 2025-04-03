c4e77aaa6b06214f17ce5e0931129d5a47b31d59
ajk9898 <ale419129@gmail.com>
2025-04-03 14:29:41 +0200

    miglioramento calendario

--- frontend/src/components/TestoCalendario.jsx
+++ frontend/src/components/TestoCalendario.jsx
@@ -1,12 +1,10 @@
 const TestoCalendario = () => {
     return (
-      <div>
         <div className="DivTestoCalendario">
-          <p className="TestoCalendario">Sacchi e contenitori dei rifiuti vanno posizionati all’esterno della propria abitazione sulla 
+          <p className="TestoCalendario">Sacchi e contenitori dei rifiuti vanno posizionati all’esterno della propria abitazione 
           <strong> sulla pubblica via, dopo le 21.00 ed entro le 24.00</strong> del giorno precedente la raccolta prevista dal calendario.
           </p>
         </div>
-      </div>
     );
   };
   
