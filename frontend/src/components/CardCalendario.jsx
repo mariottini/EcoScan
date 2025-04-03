6e9f596aecaf1d9da103849cd0b190693d8d42e7
ajk9898 <ale419129@gmail.com>
2025-04-03 17:43:49 +0200

    fine calendario e aggiunto solo comp. Chart e  npm installato

--- frontend/src/components/CardCalendario.jsx
+++ frontend/src/components/CardCalendario.jsx
@@ -3,7 +3,7 @@ const CardCalendario = () => {
 		<div className="CardCalendario">
 			{" "}
 			<img className="imgUmido" src="/img/umido.svg" alt="icona umido" />
-			<hr />
+			<hr className="RigaCalendario"/>
 			<h3 className="GenereRifiuto">Umido</h3>
 		</div>
 	);
