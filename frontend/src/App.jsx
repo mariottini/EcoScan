f21a973afe341ee9d65f8fc1d132831ae05af2a3
Jacopo-Sassi <sassijacopp@gmail.com>
2025-04-09 17:07:35 +0200

    Added 4 trash images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,11 +34,16 @@ function App() {
 				<Route path="/camera" element={[<CameraPage />, <Navbar />]} />
 				<Route path="/charts" element={[<Charts />, <Navbar />]} />
 				<Route path="/profile" element={[<Profile />, <Navbar />]} />
+<<<<<<< Updated upstream
 				<Route path="/edit-profile" element={[<EditProfile />, <Navbar />]} />
 				<Route
 					path="/trash/:name"
 					element={[<Header route={"/search"} />, <Trash />, <Navbar />]}
 				/>
+=======
+				<Route path="/trash" element={[<Header />, <Trash />, <Navbar />]} />
+				<Route path="/Modify_Profile" element={[<Modify_Profile/>, <Navbar />]} />
+>>>>>>> Stashed changes
 			</Routes>
 		</BrowserRouter>
 	);
