165e6a66fdae05e524069b03129a024a46936859
Matteo Mariottini <matteomariottini9@gmail.com>
2025-04-09 17:12:09 +0200

    Update trash page style

--- frontend/src/pages/Trash.jsx
+++ frontend/src/pages/Trash.jsx
@@ -3,11 +3,9 @@ import CardHint from "../components/CardHint";
 import CardTrash from "../components/CardTrash";
 import { useEffect, useState } from "react";
 
-
 function Trash() {
-
 	const params = useParams();
-	console.log(params.name)
+	console.log(params.name);
 
 	const [trash, setTrash] = useState({});
 
@@ -16,47 +14,50 @@ function Trash() {
 			fetch(`http://127.0.0.1:5000/get-trash/${params.name}`)
 				.then((response) => response.json())
 				.then((data) => {
-					console.log(data[0])
-					
+					console.log(data[0]);
+
 					if (data[0]) {
 						fetch(`http://127.0.0.1:5000/get-category/${data[0].id_category}`)
 							.then((response) => response.json())
 							.then((data_category) => {
-								console.log(data_category)
+								console.log(data_category);
 								data[0].name_category = data_category.name;
 
 								if (data[0]) {
 									fetch(`http://127.0.0.1:5000/get-advice/${data[0].id_trash}`)
 										.then((response) => response.json())
 										.then((data_hint) => {
-											console.log("DATA hint", data_hint)
+											console.log("DATA hint", data_hint);
 											data[0].hint_text = data_hint[0].text;
-											
+
 											setTrash(data[0]);
 										})
 										.catch((error) => console.error("Fetch error:", error));
 								}
-					
 							})
 							.catch((error) => console.error("Fetch error:", error));
 					}
-					
 				})
 				.catch((error) => console.error("Fetch error:", error));
 		}
 	}, []);
-  
-	console.log(trash)
+
+	console.log(trash);
 
 	return (
-		<div className="container_trash">
-			<img src={trash.image} alt={trash.name}></img>
-			<h2>{trash.name}</h2>
-			<section>
-				<CardHint trash={trash}/>
-				<h3>COME RICICLARE</h3>
-				<CardTrash trash={trash}/>
-			</section>
+		<div className="trash-container">
+			<div className="trash-img">
+				{/* <img src={trash.image} alt={trash.name}></img> */}
+				<img src="/img/umido.svg" alt={trash.name}></img>
+				<h2>{trash.name}</h2>
+			</div>
+			<div className="trash-info">
+				<CardHint trash={trash} />
+				<div className="card-trash-container">
+					<h3>COME RICICLARE:</h3>
+					<CardTrash trash={trash} />
+				</div>
+			</div>
 		</div>
 	);
 }
